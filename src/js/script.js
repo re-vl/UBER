7fdd93bf4034600257873c573140ab5e81a990f4
Vladimir <rva7171@gmail.com>
2021-03-17 22:29:32 +0300

    модальное окно заказать звонок

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
+window.addEventListener("DOMContentLoaded", () => {
+   //меню гамбуогер на мобильных устр-х
+   const menu = document.querySelector(".menu"),
+      menuItem = document.querySelectorAll(".menu_item"),
+      hamburger = document.querySelector(".hamburger");
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
+   hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      menu.classList.toggle("menu_active");
+   });
 
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+   menuItem.forEach((item) => {
+      item.addEventListener("click", () => {
+         hamburger.classList.toggle("hamburger_active");
+         menu.classList.toggle("menu_active");
+      });
+   });
+
+   //модальные окна
+
+   const subHeadBtn = document.querySelector(".subheader_btn"),
+      modal = document.querySelector(".modal"),
+      modalClose = modal.querySelector(".modal__close");
+
+   subHeadBtn.addEventListener("click", () => {
+      modal.classList.add("show");
+      modal.classList.remove("hide");
+   });
+
+   modalClose.addEventListener("click", () => {
+      modal.classList.add("hide");
+      modal.classList.remove("show");
+   });
+});
